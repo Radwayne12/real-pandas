2fc2f6b47b7ac1d3971b849c59ece9a7e26af30c
Radwayne12 <157649019+Radwayne12@users.noreply.github.com>
2024-06-07 00:19:27 +0200

    df.to_string csv

--- pandas-dumping.py
+++ pandas-dumping.py
@@ -2,4 +2,8 @@ import pandas as pd
 
 df = pd.read_csv('england-premier-league-players-2018-to-2019-stats.csv')
 
-print(df)
+print(df) #shows just a snippet
+
+print(df.to_string()) # will print whole csv
+
+
